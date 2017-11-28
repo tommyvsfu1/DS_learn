87b4f21e2aafc048dc9c5dc5dcd31f2d9060bd64
vsung <tommy558788@gmail.com>
2017-11-28 21:01:15 +0800

    2017/11/28

--- tree/DSHW4/DSHW4/main.cpp
+++ tree/DSHW4/DSHW4/main.cpp
@@ -11,16 +11,19 @@ class BST;
 class TreeNode {
 public:
     
-    TreeNode(int k) : leftchild(0), rightchild(0),current(0), key(k),dir('N'),x(-1){}
-    TreeNode(int k, char s, int u):leftchild(0), rightchild(0),current(0), key(k),dir(s),x(u){}
+    TreeNode(int k) : leftchild(0), rightchild(0),current(0), key(k),dir('N'),x(-1),frequency(-1){}
+    TreeNode(int k, char s, int u):leftchild(0), rightchild(0),current(0), key(k),dir(s),x(u),frequency(u){}
     ~TreeNode(){std::cout << "destructor" << std::endl;}
-    TreeNode():leftchild(0),rightchild(0),current(0),key(-1),dir('N'),x(-1){}
+    TreeNode():leftchild(0),rightchild(0),current(0),key(-1),dir('N'),x(-1),frequency(-1){}
     TreeNode(const TreeNode& p) {
         std::cout << "copy constructor!" << std::endl;
         this->current = p.current;
         this->key = p.key;
         this->leftchild = p.leftchild;
         this->rightchild = p.rightchild;
+        this->frequency = p.frequency;
+        this->x = p.x;
+        this->dir = p.dir;
     }
     
     char Update();
@@ -32,6 +35,7 @@ private:
     int key;
     char dir;
     int x;
+    int frequency;
 };
 
 char TreeNode::Update(){
@@ -41,12 +45,13 @@ char TreeNode::Update(){
     //==============
     
     static char cb;
-    if (x == 0 ) return this->dir;
+    if (x == 0 && frequency == 0) return this->dir;
     --x;
     cb = this->dir;
     if (x == 0) {
         if (this->dir == 'R') this->dir = 'L';
         else if(this->dir == 'L') this->dir = 'R';
+        x = frequency;
     }
     
     //std::cout << this->key << " " << std::endl; //homework need to print
@@ -59,26 +64,54 @@ class BST {
 public:
     BST(TreeNode* n):root(n){}
     void Insert(TreeNode*);
-    TreeNode* Inorder(TreeNode* node) {
+    void Inorder(TreeNode* node) {
         if (node) {
             Inorder(node->leftchild);
             std::cout << node->key << " " << node->dir << " " << node->x << std::endl;
             Inorder(node->rightchild);
         }
-        return 0;
+        return;
     }
     void Delete(int key);
     void Drop(int number);
+    TreeNode* maxchild(TreeNode*);
+    TreeNode* minchild(TreeNode*);
     TreeNode* GetRoot() {return root;}
     
 private:
     TreeNode* root;
 };
 
+
+TreeNode* BST::maxchild(TreeNode* n) {
+    // this node should not be a null pointer, because this case is the two child case
+    TreeNode* q;
+    for(static TreeNode* k = n;;k = k->rightchild) {
+        if (k->rightchild == 0) {
+            q->rightchild = 0;
+            return k;
+        }
+        q = k;
+    }
+}
+
+TreeNode* BST::minchild(TreeNode* n) {
+    // this node should not be a null pointer, because this case is the two child case
+    TreeNode* q;
+    for(static TreeNode* k = n;;k = k->leftchild) {
+        if (k->leftchild == 0) {
+            q->leftchild = 0;
+            return k;
+        }
+        q = k;
+    }
+    
+}
+
 void BST::Insert(TreeNode* t) {
     if (!root) return ;
     //TreeNode* p = new TreeNode;
-    TreeNode* q = new TreeNode;
+    TreeNode* q;
     for (TreeNode* p = root; p;) {
         q = p;
         if (t->key == p->key){ // if id exists, replace
@@ -92,15 +125,14 @@ void BST::Insert(TreeNode* t) {
     }
     
     // link to the right parent
-    TreeNode* p = new TreeNode;
+    // assign the data
+    TreeNode* p = new TreeNode(*t); //copy constructor
     //p->key = t->key;
     if (q->key > t->key) q->leftchild = p;
     else q->rightchild = p;
     
-    // assign the data
-    p->key = t->key;
-    p->dir = t->dir;
-    p->x = t->x;
+
+
     
 }
 
@@ -108,38 +140,54 @@ void BST::Delete(int k) {
     //start searching first, check root
     if (!root) {std::cout<< "empty tree"; return ;}
     
-    TreeNode* q = new TreeNode;
-    TreeNode* p  = new TreeNode;
+    TreeNode* q = 0;
+    TreeNode* p = 0;
     //delete p;
     for (p = root; p;) {
-        q = p;
+      
         if (p->key == k) break;
+        //=====this line I think is putting below
+        q = p;
         if (p->key < k) p = p->rightchild;
         else p = p -> leftchild;
     }
     
-    if (!p){ std::cout << "key doesn't exist"; return ;}
+    if (!p){ std::cout << "Deletion failed."; return ;}
     else {
         
-        // p is TreeNode
+        // p is TreeNode need to be deleted
         if (!p->leftchild && !p->rightchild) {
+            if (q->key < k) q->rightchild = 0;
+            else q->leftchild = 0;
             delete p;
-            std::cout << "fucking I delete it"<< std::endl;
-            std::cout << p->key;
-            return ;}
+            //std::cout << p->key;
+            return ;
+        }
         
-        //no child //don't know why can't delete
         else if(p->leftchild && p->rightchild) { // two child or say two subtree
-            
+            if (q->key < k) {
+                q->rightchild = maxchild(p);
+                q->rightchild->leftchild = p->leftchild;
+                q->rightchild->rightchild = p->rightchild;
+                delete p;
+            }
+            else {
+                q->leftchild = maxchild(p);
+                q->leftchild->leftchild = p->leftchild;
+                q->leftchild->rightchild = p->rightchild;
+                delete p;
+            }
         }
         else {
             if (p->leftchild) {
                 if (p->key < q->key) {q->leftchild = p->leftchild;}
                 else {q->rightchild = p->leftchild;}
+                delete p;
             }
             else {
                 if (p->key < q->key) {q->leftchild = p->rightchild;}
                 else {q->rightchild = p->rightchild;}
+                delete p;
             }
         }
     }
@@ -168,22 +216,25 @@ void BST::Drop(int number) {
 
 int main(int argc, const char * argv[]) {
     
-    TreeNode* k = new TreeNode(3,'R',3);
-    TreeNode* p1 = new TreeNode(4,'L',1);
-    TreeNode* p2 = new TreeNode(13,'R',5);
-    TreeNode* p3 = new TreeNode(1,'R',2);
-    TreeNode* p4 = new TreeNode(15,'R',1);
-    TreeNode* p5 = new TreeNode(5,'L',5);
-    TreeNode* p6 = new TreeNode(7,'L',2);
-    BST T(k);
-    T.Insert(p1);
-    T.Insert(p2);
-    T.Insert(p3);
-    T.Insert(p4);
-    T.Insert(p5);
-    T.Insert(p6);
+    TreeNode k(3,'R',2);
+    TreeNode p1(4,'L',1);
+    TreeNode p2(13,'R',5);
+    TreeNode p3(1,'R',2);
+    TreeNode p4(15,'R',1);
+    TreeNode p5(5,'L',5);
+    TreeNode p6(7,'L',2);
+    BST T(&k);
+    T.Insert(&p1);
+    T.Insert(&p2);
+    T.Insert(&p3);
+    T.Insert(&p4);
+    T.Insert(&p5);
+    T.Insert(&p6);
     T.Inorder(T.GetRoot());
-    T.Delete(15);
+    //T.Delete(13);
+    //T.Inorder(T.GetRoot());
+    
+    T.Drop(2);
     T.Inorder(T.GetRoot());
     //int* qq = new int;
     //delete qq;
